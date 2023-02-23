28f9b7598a014a28a26e9f58e558eb05f77c3cae
Taka005 <01taka005@gmail.com>
2023-02-23 04:12:55 +0000

    修正

--- index.js
+++ index.js
@@ -8,43 +8,15 @@ const getURL = async(ID)=>{
         "headers":{
             "Content-Type": "application/json"
         },
-        "body": JSON.stringify(
-            {  
-                "context":{
-                    "client":{
-                        "hl": "ja",
-                        "clientName": "WEB",
-                        "clientVersion": "2.20210721.00.00",
-                        "clientFormFactor": "UNKNOWN_FORM_FACTOR",
-                        "clientScreen": "WATCH",
-                        "mainAppWebInfo":{
-                            "graftUrl": `/watch?v=${ID}`,
-                        }
-                    },
-                    "user":{
-                        "lockedSafetyMode": false
-                    },
-                    "request":{
-                        "useSsl": true,
-                        "internalExperimentFlags": [],
-                        "consistencyTokenJars": []
-                    }
-                },
-                "videoId": ID,  
-                "playbackContext":{
-                    "contentPlaybackContext":{
-                        "vis": 0,
-                        "splay": false,
-                        "autoCaptionsDefaultOn": false,
-                        "autonavState": "STATE_NONE",
-                        "html5Preference": "HTML5_PREF_WANTS",
-                        "lactMilliseconds": "-1"
-                    }
-                },
-                "racyCheckOk": false,
-                "contentCheckOk": false
-            }    
-        )
+        "body": JSON.stringify({  
+            "context":{
+                "client":{
+                    "clientName": "WEB",
+                    "clientVersion": "2.20210721.00.00",
+                }
+            },
+            "videoId": ID
+        })
     })
     .then(res=>res.json())
     .catch(err=>console.log(err))
